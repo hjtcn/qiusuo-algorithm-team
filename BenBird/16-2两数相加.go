de83a50fc43276eb9c3a9293b15fcbdf8b4a254e
BenBird <hjt1160986096@gmail.com>
2020-09-29 11:27:56 +0800

    两数相加题解

--- BenBird/16-2两数相加.go
+++ BenBird/16-2两数相加.go
@@ -31,6 +31,7 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	l3 := &ListNode{0, nil}
 	curr := l3
 	carry := 0
+	sum := 0
 
 	for l1 != nil || l2 != nil {
 		num1, num2 := 0, 0
@@ -45,7 +46,7 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 			l2 = l2.Next
 		}
 
-		sum := num1 + num2 + carry
+		sum = num1 + num2 + carry
 
 		if sum >= 10 {
 			carry = sum / 10
@@ -58,6 +59,7 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 		curr = curr.Next
 	}
 
+	//当链表末尾需要进位时，继续创建一个新节点
 	if carry != 0 {
 		curr.Next = &ListNode{carry, nil}
 	}
@@ -69,3 +71,32 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 func main()  {
 
 }
+
+/**
+题解：两数相加
+
+耗时：12ms
+内存：4.9M
+
+注意：
+	进位问题
+		链表末尾进位
+		非链表末尾进位
+	两链表长度不一问题
+
+进位问题可设置一个进位变量 carry，每次判断carry是否为 0，来辨别是否有进位
+
+首先遍历两个链表，只要存在一个链表未到末尾，就继续遍历
+	遍历时将链表的两个数值 num1、num2、carry进行相加，得sum
+	判断sum 是否大于 9
+		大于，则需要求进位数 carry = sum / 10; sum = sum % 10
+		小于，进位数 carry = 0; sum = sum
+
+	然后创建一个新节点，val = sum, next = nil
+
+直到最后一个链表完成遍历，即可得到一个新的链表，返回
+
+复杂度分析：
+	时间复杂度：O(max(m, n))
+	空间复杂度：O(n)
+ */
